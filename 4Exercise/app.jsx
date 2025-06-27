7d51c97e26b08b1f86366a9dcf281645d6705664
Benjamín Fernández <benji.p.fb@gmail.com>
2025-06-27 14:44:21 -0300

    rename of the archives, and four exercise done

---
+++ 4Exercise/app.jsx
@@ -0,0 +1,28 @@
+const App = () => {
+  const [enabled, setEnabled] = React.useState("left");
+
+  const handleClick = (side) => {
+    setEnabled(side === "left" ? "right" : "left");
+  };
+
+  return (
+    <>
+      <h1>alternatives buttons</h1>
+      <button
+        onClick={() => handleClick("left")}
+        disabled={enabled !== "left"}
+      >
+        Lefty
+      </button>
+      <button
+        onClick={() => handleClick("right")}
+        disabled={enabled !== "right"}
+      >
+        Righty
+      </button>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
