7468df37954c738d39b484d174262a922c572612
Benjamín Fernández <benji.p.fb@gmail.com>
2025-06-27 14:04:49 -0300

    third exercise

---
+++ ThirdExercise/javascript.js
@@ -0,0 +1,34 @@
+const todosEndpoint = "https://jsonplaceholder.typicode.com/todos?_limit=3"
+const loading = document.getElementById("loading")
+const BTNcharge = document.getElementById("BTN-charge")
+const error = document.getElementById("error")
+const list = document.getElementById("completedToDosUl")
+
+BTNcharge.addEventListener("click", async () => {
+  list.innerHTML = ""
+  loading.style.display = "block"
+
+  const response = await fetch(todosEndpoint)
+
+  loading.style.display = "none"
+
+  if (!response.ok) {
+    error.textContent = `it was a problem: ${response.status} - ${response.statusText}`
+    return
+  }
+
+  const data = await response.json()
+
+  const completedToDos = data.filter(todo => todo.completed === true)
+
+  if (completedToDos.length > 0) {
+    completedToDos.forEach(todo => {
+      const li = document.createElement("li")
+      li.textContent = `The id of the todo: ${todo.id}, the title: ${todo.title} `
+      list.appendChild(li)
+    }
+    );
+  } else {
+    list.innerHTML = "<strong>There aren't any completed todos</strong>"
+  }
+})
